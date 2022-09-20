80f5404819df444b2f205bf99612df5315db28d6
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-20 19:05:10 +0400

    lesson1 task8

---
+++ lesson1_task8/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while(count <= N)
+{
+    if(count%2 == 0) 
+    {
+        Console.WriteLine(count);
+        count = count + 2;
+    }
+}
+
