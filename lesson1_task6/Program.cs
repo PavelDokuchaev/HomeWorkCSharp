60d7b2390113e4523d7da0fa89a896e9ef3801c8
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-20 15:09:45 +0400

    lesson1 task6

---
+++ lesson1_task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+    {
+        Console.WriteLine("Введенное вами число четное");
+    }
+else
+    {
+        Console.WriteLine("Введенное вами число нечетное");    
+    }
