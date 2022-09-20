6fa2ed703739393733546647ede7383cb3e3e922
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-20 14:07:30 +0400

    lesson1 task2

---
+++ lesson1_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());;
+
+if(number1 == number2)
+    {
+        Console.WriteLine("Введите разные числа");
+    }
+else
+{
+    if(number1 > number2)
+    {
+    Console.WriteLine($"Большее число: {number1}");
+    Console.WriteLine($"Меньшее число: {number2}");
+    }   
+    else
+    Console.WriteLine($"Большее число: {number2}");
+    Console.WriteLine($"Меньшее число: {number1}");
+}
